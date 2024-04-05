f94d4e8858c8ef3970f6c98579b58c0457f3e630
choichangyeon <changyon99@naver.com>
2024-04-05 22:08:14 +0900

    make FE branch

---
+++ frontend/lib/screens/home/shorts/shorts_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShortsScreen extends StatelessWidget {
+  const ShortsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        '쇼츠 들어갈 페이지',
+        style: TextStyle(
+          fontSize: 50,
+        ),
+      ),
+    );
+  }
+}
