05afbc67513e88bff0ca33be2b3a58384bd8c75b
ji hwan <jjhinu1004@gmail.com>
2024-04-19 00:26:13 +0900

    #6 feat: auth에서의 예외처리 및 토큰 예외처리

--- backend/src/main/java/com/project/capstone/auth/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/project/capstone/auth/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,12 @@
 package com.project.capstone.auth.jwt;
 
 
+import com.project.capstone.auth.exception.AuthException;
+import com.project.capstone.member.exception.MemberException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,10 +15,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static com.project.capstone.auth.exception.AuthExceptionType.*;
+import static com.project.capstone.member.exception.MemberExceptionType.*;
 
 @RequiredArgsConstructor
 @Component
@@ -29,8 +39,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             Authentication authentication = jwtProvider.createAuthentication(id);
             log.info(authentication.getName());
             SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (SecurityException e) {
+            request.setAttribute("exception", new AuthException(SIGNATURE_NOT_FOUND));
+        } catch (SignatureException e) {
+            request.setAttribute("exception", new AuthException(SIGNATURE_INVALID));
+        } catch (MalformedJwtException e) {
+            request.setAttribute("exception", new AuthException(MALFORMED_TOKEN));
+        } catch (ExpiredJwtException e) {
+            request.setAttribute("exception", new AuthException(EXPIRED_TOKEN));
+        } catch (UnsupportedJwtException e) {
+            request.setAttribute("exception", new AuthException(UNSUPPORTED_TOKEN));
+        } catch (IllegalArgumentException e) {
+            request.setAttribute("exception", new AuthException(INVALID_TOKEN));
+        } catch (UsernameNotFoundException e) {
+            request.setAttribute("exception", new MemberException(MEMBER_NOT_FOUND));
         } catch (Exception e) {
-            log.warn(e.getMessage());
+            request.setAttribute("exception", new Exception());
         }
 
         filterChain.doFilter(request, response);
