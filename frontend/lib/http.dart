3b74497eca6e33be321f6fd2ee9d2195d10f0c5b
choichangyeon <changyon99@naver.com>
2024-05-05 00:44:50 +0900

    Search Book Flow make

--- frontend/lib/http.dart
+++ frontend/lib/http.dart
@@ -2,6 +2,40 @@ import 'package:http/http.dart' as http;
 import 'package:frontend/secret.dart';
 import 'dart:convert';
 
+const String NaverBookSearchURL =
+    "https://openapi.naver.com/v1/search/book.json";
+
+//네이버 책 검색
+Future<List<dynamic>> SearchBook(String SearchString) async {
+  List<dynamic> bookData = [];
+  int cnt = 0, iter = 0;
+  var address = Uri.parse(NaverBookSearchURL + "?query=$SearchString&display=100");
+  http.Response res = await http.get(address, headers: {
+    "Content-Type": "application/json",
+    "X-Naver-Client-Id": NaverClientID,
+    "X-Naver-Client-Secret": NaverSecret,
+  });
+  final data = json.decode(utf8.decode(res.bodyBytes));
+  // for(int i = 0; i < 10; i++){  
+  //   bookData.add(data['items'][i]);
+  //   print(data['items'][i]);
+  // }
+  while(cnt != 10){
+    if(data['items'][iter]['title'].toString().contains('시리즈')){
+      iter++;
+      continue;
+    }
+    else{
+      bookData.add(data['items'][iter]);
+      // print(data['items'][iter]);
+      iter++;
+      cnt++;
+    }
+  }
+  
+  return bookData;
+}
+
 //회원 가입
 Future<String> singup(String email, String name, int age, String gender) async {
   var address = Uri.parse(BASE_URL + "/auth/signup");
@@ -42,26 +76,29 @@ Future<String> login(String email) async {
 }
 
 //모임 목록 가져오기(주제를 기반으로)›
-Future<List<dynamic>> groupSerachforTopic(String topic) async {
-  List<dynamic> groupList = [];
-  var address = Uri.parse(BASE_URL + "/club/search/topic?topic=$topic");
-  http.Response res = await http.get(
-    address,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  );
-  final data = json.decode(utf8.decode(res.bodyBytes));
-  for (int i = 0; i < data.length; i++) {
-    groupList.add(data[i]);
-    // print(data[i]);
+Future<List<List<dynamic>>> groupSerachforTopic(List<String> topic) async {
+  List<List<dynamic>> groupList = [[], [], [], [], []];
+  for (int i = 0; i < topic.length; i++) {
+    String temp = topic[i];
+    var address = Uri.parse(BASE_URL + "/club/search/topic?topic=$temp");
+    http.Response res = await http.get(
+      address,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    );
+    final data = json.decode(utf8.decode(res.bodyBytes));
+    for (int j = 0; j < data.length; j++) {
+      groupList[i].add(data[j]);
+    }
   }
 
   return groupList;
 }
 
 //모임 생성하기
-Future<dynamic> groupCreate(dynamic token,  String name, String topic, int maximum, String publication, var password) async {
+Future<dynamic> groupCreate(dynamic token, String name, String topic,
+    int maximum, String publication, var password) async {
   var address = Uri.parse(BASE_URL + "/club/create");
   http.Response res = await http.post(
     address,
--- frontend/lib/screens/home/search/search_screen.dart
+++ frontend/lib/screens/home/search/search_screen.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:frontend/http.dart';
+import 'package:frontend/screens/home/search/search_screen_util.dart'
+    as SearchUtil;
 
 List<dynamic> token = [];
-
+List<dynamic> BookData = [];
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -14,11 +16,42 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchState extends State<SearchScreen> {
+  final TextEditingController _textControllers = TextEditingController();
+
+  void _SearchData(String text) async {
+    BookData = await SearchBook(text);
+    setState(() {});
+  }
+
+  bool _isFieldEmpty(TextEditingController controller) {
+    return controller.text.trim().isEmpty;
+  }
+
+  void initiate() {
+    //초기화 함수
+    setState(() {
+      BookData = [];
+      _textControllers.clear();
+    });
+  }
+
+  @override
+  void dispose() {
+    _textControllers.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initiate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(390, 675),
-      builder:(context, child) => Scaffold(
+      builder: (context, child) => Scaffold(
         appBar: AppBar(
           scrolledUnderElevation: 0,
           backgroundColor: const Color(0xFF0E9913),
@@ -37,48 +70,75 @@ class _SearchState extends State<SearchScreen> {
           child: Column(
             children: [
               Container(
-                width: double.infinity,
-                height: 120.h,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF0E9913),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
+                  width: double.infinity,
+                  height: ScreenUtil().setHeight(120),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF0E9913),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
                   ),
-                ),
-              ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _textControllers,
+                              decoration: const InputDecoration(
+                                hintText: '책 제목',
+                              ),
+                              onChanged: (value) {
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              if(_isFieldEmpty(_textControllers)){
+                                // 검색 x
+                                // BookData = [];
+                              }
+                              else{
+                                _SearchData(_textControllers.text);
+                              }
+                              setState(() {
+                                
+                              });
+                            },
+                            icon: const Icon(Icons.search),
+                          )
+                        ],
+                      ),
+                    ],
+                  )),
               Expanded(
                 child: SingleChildScrollView(
                   scrollDirection: Axis.vertical,
                   child: Column(
                     children: [
                       ElevatedButton(
-                        onPressed: (){
-                          context.push(
-                            '/book_info',
-                            extra: '썸띵',
-                          );
-                        }, 
-                        child: Text('test'),
-                      ),
-                      ElevatedButton(
-                        onPressed: ()async{
+                        onPressed: () async {
                           token = [];
                           token.add(singup("test4@gmail.com", "최연습", 33, "여자"));
                           token.add(singup("test5@gmail.com", "한연습", 37, "남자"));
                           token.add(singup("test6@gmail.com", "석연습", 46, "여자"));
-                        }, 
+                        },
                         child: Text('회원가입'),
                       ),
                       ElevatedButton(
-                        onPressed: ()async{
+                        onPressed: () async {
                           token = [];
                           token.add(login("test4@gmail.com"));
                           token.add(login("test5@gmail.com"));
                           token.add(login("test6@gmail.com"));
-                        }, 
+                        },
                         child: Text('로그인'),
                       ),
+                      if (BookData.isNotEmpty)
+                        for (int i = 0; i < BookData.length; i++)
+                          SearchUtil.SearchListItem(data:BookData[i])
                     ],
                   ),
                 ),
